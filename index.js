1e13ba354ecbf8fdfdac1c5b4009b503c9448353
Likogan <likogann@gmail.com>
2023-02-22 13:04:13 -0700

    Move generateCaptchav2.js directly into index.js

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 module.exports = { genCaptcha, genCaptchav2, genImgFromBuffer };
 
-// generateCaptcha.js
+
+// # Captcha Generation
+// ## generateCaptcha.js
 function genCaptcha(config) {
   let genCaptcha = require('./src/generateCaptcha.js');
   return genCaptcha(config);
 };
 
-// generateCaptchav2.js
+// ## generateCaptchav2.js
 function genCaptchav2() {
-  let genCaptchav2 = require('./src/generateCaptchav2.js');
-  return genCaptchav2();
+  // Generate a background colour. We cannot do this inside var config ...
+  // as it's used twice
+  bgColour = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)});
+  const config = {
+      // ## Image
+      width: 400,
+      height: 200,
+      bgColour: bgColour,
+      realTextColour: bgColour,
+      // ## Obfuscation
+      lineWidth: 2,
+      lines: 100,
+      shapes: genRandom(10, 40),
+      shapesSizeMultiplier: 40,
+      // ## Text size, length, and rotation
+      fontsize: 60,
+      charLength: 6,
+      rotatemax: 20,
+      rotatemin: -20
+  }
+
+  // run genCaptcha with the custom config.
+  return genCaptcha(config)
+  // genCaptchaV2 is more of an example of how changing the config can give
+  // some creative results.
 };
 
-// generateImageFromBuffer.js
+// # Captcha Translation
+// ## generateImageFromBuffer.js
 function genImgFromBuffer(buf, path) {
   let genImgFromBuffer = require('./src/generateImageFromBuffer.js');
   return genImgFromBuffer(buf, path);
+};
+
+// # Internal Functions
+// ## GenRandom function
+function genRandom(min, max) {
+  return Math.random() * (max - min) + min;
 };
